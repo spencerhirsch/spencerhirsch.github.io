23933c63341525c732a7b7bed62ecc5bb75a120d
Spencer Hirsch <spencer@yellowcrest.com>
2024-06-27 20:05:26 -0400

    trying to fix issues?

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer>
       <ul>
-        <li><a href="https://www.linkedin.com/in/yourprofile" target="_blank">LinkedIn</a></li>
-        <li><a href="https://github.com/yourprofile" target="_blank">GitHub</a></li>
+        <p>&copy; 2024 Spencer Hirsch. All rights reserved.</p>
       </ul>
     </footer>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa'; // Import icons from react-icons library
+import './Header.css';
 
 const Header = () => {
   return (
     <header>
-      <nav>
+      <div className="left">
+        <h1 classname="name">Spencer Hirsch</h1>
+      </div>
+      <nav className="middle">
         <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/experience">Experience</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/resume">Resume</Link></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/experience">Experience</NavLink></li>
+          <li><NavLink to="/projects">Projects</NavLink></li>
+          <li><NavLink to="/resume">Resume</NavLink></li>
         </ul>
       </nav>
+      <div className="right">
+        <a href="https://github.com/spencerhirsch" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/hirsch-spencer" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+        <a href="https://instagram.com/spencerhirsch" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+      </div>
     </header>
   );
 };
